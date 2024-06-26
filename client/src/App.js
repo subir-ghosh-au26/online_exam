8aa98e533092914c74c1dcb9bdd8afc1647e11ee
Subir Ghosh <subirsgghosh@gmail.com>
2024-06-26 10:55:36 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+// import { Button } from 'antd';
+import './stylesheets/theme.css';
+import './stylesheets/textelements.css';
+import './stylesheets/alignments.css';
+import './stylesheets/custom-components.css';
+import './stylesheets/form-elements.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/common/login';
+import Register from './pages/common/register';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
---
+++ client/src/pages/common/login/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Form } from 'antd'
+import { Link } from 'react-router-dom'
+function Login() {
+
+    const onFinish = (values) => {
+        console.log(values)
+    }
+
+
+    return (
+        <div className='flex justify-center item-center h-screen w-screen'>
+            <div className="card w-400 p-3">
+                <div className="flex flex-col">
+                    <h1 className="text-2xl">
+                        Login
+                    </h1>
+                    <div className="divider"></div>
+                    <Form layout='vertical' className='mt-2' onFinish={onFinish}>
+                        <Form.Item
+                            name='email'
+                            label='Email'
+                        >
+                            <input type="text" />
+                        </Form.Item>
+                        <Form.Item
+                            name='password'
+                            label='Password'
+                        >
+                            <input type="password" />
+                        </Form.Item>
+                        <div className="flex flex-col gap-2">
+                            <button type="submit" className='primary-contained-btn mt-2 w-100'>Login</button>
+                            <Link to="/register" className='text-center mt-2 underline'>Not a Member? Register</Link>
+                        </div>
+                    </Form>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Login
